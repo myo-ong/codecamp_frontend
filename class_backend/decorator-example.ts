2931a236f9edd9960731522275cd10e03bd05b0a
myo-ong <myo.ongzzz@gmail.com>
2023-07-22 09:34:59 +0900

    first commit

---
+++ class_backend/decorator-example.ts
@@ -0,0 +1,11 @@
+function bbb(qqqqqqq: any) {
+  console.log("===========");
+  console.log(qqqqqqq);
+  console.log("==========");
+}
+
+@bbb
+class Board {
+  // number: number
+  // writer: string
+}
---
+++ class_backend/index.ts
@@ -0,0 +1,113 @@
+// const qqq: string = "안녕하세요~";
+// console.log(qqq);
+
+import { DataSource } from "typeorm";
+import { Board } from "./Board.postgres";
+
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+
+// API-Docs 만들기
+const typeDefs = `#graphql
+input CreateBoardInput {
+  writer: String
+  title: String
+  contents: String
+}
+  type MyBoard {
+    number: Int
+    writer: String
+    title: String
+    contents: String
+  }
+
+  type Query {
+    fetchBoards: [MyBoard]
+  }
+
+  type Mutation {
+    # 연습용(backend-example 방식)
+    # createBoard(writer: String, title: String, contents: String): String
+
+    # 실무용(backend-practice 방식)
+    createBoard(createBoardInput: CreateBoardInput): String
+  }
+`;
+
+// API만들기
+const resolvers = {
+  Query: {
+    fetchBoards: async () => {
+      // 1. 모두 꺼내기
+      const result = await Board.find();
+      console.log(result);
+
+      // 2. 한 개만 꺼내기
+      // const result = await Board.findOne({ where: { number: 3 } });
+      // console.log(result);
+
+      return result;
+    },
+  },
+
+  Mutation: {
+    createBoard: async (parent: any, args: any, context: any, info: any) => {
+      await Board.insert({
+        ...args.createBoardInput,
+
+        // 하나 하나 모두 입력하는 비효율적인 방식
+        // writer: args.createBoardInput.writer,
+        // title: args.createBoardInput.title,
+        // contents: args.createBoardInput.contents,
+      });
+
+      return "게시글 등록에 성공했어요.";
+    },
+
+    // updateBoard: async () => {
+    //   // 3번 게시글을 영희로 바꿔줘
+    //   await Board.update({ number: 3 }, { writer: "영희" });
+    // },
+
+    // deleteBoard: async () => {
+    //   await Board.delete({ number: 3 }); // 3번 게시글 삭제해줘
+    //   await Board.update({ number: 3 }, { isDeleted: true }); // 3번 게시글 삭제했다치자 (소프트삭제) => isDeleted가 초기값이 false이면? 삭제 안된거, true이면? 삭제 된거
+    //   await Board.update({ number: 3 }, { deletedAt: new Date() }); // 3번 게시글 삭제했다치자 (소프트삭제) => deletedAt 초기값이 NULL이면? 삭제 안된거, new Date()가 들어가있으면? 삭제 된거
+    // },
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+
+  // 선택한 사이트만 풀어주고 싶을 때
+  // cors: {
+  //   origin: ["http://naver.com", "http://coupang.com"],
+  // },
+});
+
+const AppDataSource = new DataSource({
+  type: "postgres",
+  host: "34.64.244.122",
+  port: 5001,
+  username: "postgres",
+  password: "postgres2022",
+  database: "postgres",
+  entities: [Board],
+  synchronize: true,
+  logging: true,
+});
+
+AppDataSource.initialize()
+  .then(() => {
+    console.log("DB접속에 성공했습니다!");
+
+    startStandaloneServer(server).then(() => {
+      console.log("그래프큐엘 서버가 실행되었습니다."); // 포트: 4000
+    });
+  })
+  .catch((error) => {
+    console.log("DB접속에 실패했습니다!");
+    console.log("원인: ", error);
+  });
